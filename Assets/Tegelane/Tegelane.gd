2be78782f08a8d373ea97b60ce6fc33a78c541f4
Taiger Kala <taiger.kala@gmail.com>
2023-04-25 11:02:06 +0300

    Pühapäevane töö

--- Assets/Tegelane/Tegelane.gd
+++ Assets/Tegelane/Tegelane.gd
@@ -1,6 +1,9 @@
 extends KinematicBody2D
 
-onready var sprite := $Pilt
+onready var animation_player := $AnimationPlayer
+onready var pilt := $AnimatedSprite
+onready var animation_tree := $AnimationTree
+onready var state_machine = animation_tree.get("parameters/playback")
 
 const SPEED := 700.0
 
@@ -10,4 +13,22 @@ func _physics_process(delta: float) -> void:
 	move_and_slide(velocity)
 	
 	if direction:
-		sprite.flip_h = sign(direction.x) == -1
+		state_machine.travel("move")
+		pilt.flip_h = sign(direction.x) == -1
+	else:
+		state_machine.travel("idle")
+	
+	if Input.is_action_just_pressed("atk_slam"):
+		state_machine.travel("atk_slam")
+	if Input.is_action_just_pressed("atk_slash"):
+		state_machine.travel("atk_slash")
+	if Input.is_action_just_pressed("atk_spin"):
+		state_machine.travel("atk_spin")
+		
+func stop_physics():
+	set_physics_process(false)
+	return 
+
+func start_physics():
+	set_physics_process(true)
+	return
