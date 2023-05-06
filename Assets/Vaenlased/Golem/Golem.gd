a885ce2594dbd8dd058dd29acde1a023ac3f824e
Taiger Kala <taiger.kala@gmail.com>
2023-05-06 13:52:25 +0300

    Update

--- Assets/Vaenlased/Golem/Golem.gd
+++ Assets/Vaenlased/Golem/Golem.gd
@@ -7,6 +7,8 @@ var velocity := Vector2.ZERO
 var target: KinematicBody2D
 
 onready var aggro_area := $AggroArea
+onready var animation_tree := $AnimationTree
+onready var state_machine = animation_tree.get("parameters/playback")
 
 func _ready() -> void:
 	aggro_area.connect("body_entered", self, "_on_player_entered")
@@ -15,9 +17,11 @@ func _ready() -> void:
 func _physics_process(delta: float) -> void:
 	#Golemi liikumine
 	var direction := Vector2.ZERO
+	state_machine.travel("idle")
 	
 	if target:
 		direction = to_local(target.global_position).normalized()
+		state_machine.travel("move")
 
 	#Suuna muutus
 	var desired_velocity := max_speed * direction
@@ -33,3 +37,6 @@ func _on_player_entered(enemy: KinematicBody2D) -> void:
 func _on_player_exited(enemy: KinematicBody2D) -> void:
 	target = null
 	
+func atk_enemy():
+	state_machine.travel("atk_punch")
+
